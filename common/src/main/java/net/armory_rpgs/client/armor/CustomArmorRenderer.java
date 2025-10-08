8cf9ac6017d64a1cfb12265c83d606c0b6df225f
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-10-08 18:28:54 +0200

    Add justicar trims

--- common/src/main/java/net/armory_rpgs/client/armor/CustomArmorRenderer.java
+++ common/src/main/java/net/armory_rpgs/client/armor/CustomArmorRenderer.java
@@ -3,6 +3,7 @@ package net.armory_rpgs.client.armor;
 import mod.azure.azurelibarmor.rewrite.render.AzRendererConfig;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelibarmor.rewrite.render.layer.AzArmorTrimLayer;
 import mod.azure.azurelibarmor.rewrite.render.layer.AzAutoGlowingLayer;
 import net.armory_rpgs.ArmoryMod;
 import net.minecraft.item.ItemStack;
@@ -14,7 +15,7 @@ public class CustomArmorRenderer extends AzArmorRenderer {
     }
 
     public static CustomArmorRenderer justicar_armor() {
-        return new CustomArmorRenderer("justicar_armor", "justicar_armor");
+        return new CustomArmorRenderer("justicar_armor", "justicar_armor", "justicar_armor_generic");
     }
 
     public static CustomArmorRenderer destroyer_armor() {
@@ -34,18 +35,18 @@ public class CustomArmorRenderer extends AzArmorRenderer {
     }
 
     public static CustomArmorRenderer astral_robe() {
-        return trisifal("astral_robe");
+        return tirisfal("astral_robe");
     }
 
     public static CustomArmorRenderer scarlet_robe() {
-        return trisifal("scarlet_robe");
+        return tirisfal("scarlet_robe");
     }
 
     public static CustomArmorRenderer glacier_robe() {
-        return trisifal("glacier_robe");
+        return tirisfal("glacier_robe");
     }
 
-    private static CustomArmorRenderer trisifal(String texture) {
+    private static CustomArmorRenderer tirisfal(String texture) {
         return new CustomArmorRenderer("tirisfal_robe", texture);
     }
 
@@ -53,7 +54,16 @@ public class CustomArmorRenderer extends AzArmorRenderer {
         super(AzArmorRendererConfig.builder(
                 Identifier.of(ArmoryMod.NAMESPACE, "geo/" + modelName + ".geo.json"),
                 Identifier.of(ArmoryMod.NAMESPACE, "textures/armor/" + textureName + ".png"))
+                .build()
+        );
+    }
+
+    public CustomArmorRenderer(String modelName, String textureName, String trimTexture) {
+        super(AzArmorRendererConfig.builder(
+                        Identifier.of(ArmoryMod.NAMESPACE, "geo/" + modelName + ".geo.json"),
+                        Identifier.of(ArmoryMod.NAMESPACE, "textures/armor/" + textureName + ".png"))
                 .addRenderLayer(new AzAutoGlowingLayer<>())
+                .addRenderLayer(new AzArmorTrimLayer(Identifier.of(ArmoryMod.NAMESPACE, "armor/trim/" + trimTexture), false))
                 .build()
         );
     }
